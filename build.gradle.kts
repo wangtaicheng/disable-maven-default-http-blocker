c23bd848851b3d514b8f6819ed2a661bda4c4792
王太成 <wtchxx-223@163.com>
2026-09-10 14:33:43 +0800

    feat: 新增禁用Maven默认HTTP拦截器的IntelliJ插件

---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "2.0.20"
+    id("org.jetbrains.intellij.platform") version "2.18.1"
+}
+
+group = "io.github.cdgeass"
+version = "2.0"
+
+repositories {
+    mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+dependencies {
+    intellijPlatform {
+        // The plugin is platform-only and depends solely on com.intellij.modules.platform,
+        // so it is compatible with IntelliJ IDEA Ultimate (IU-262.*) and other IDEs.
+        intellijIdea("2026.2")
+    }
+}
+
+intellijPlatform {
+    pluginConfiguration {
+        ideaVersion {
+            sinceBuild = "262"
+            untilBuild = "262.*"
+        }
+    }
+}
+
+tasks {
+    withType<JavaCompile> {
+        options.encoding = "UTF-8"
+    }
+    withType<KotlinCompile> {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_21
+        }
+    }
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
---
+++ src/main/kotlin/io/github/cdgeass/dmdhb/DisableMavenDefaultBlockerActivity.kt
@@ -0,0 +1,63 @@
+package io.github.cdgeass.dmdhb
+
+import com.intellij.openapi.application.PathManager
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.startup.ProjectActivity
+import org.w3c.dom.CharacterData
+import java.io.File
+import javax.xml.parsers.DocumentBuilderFactory
+import javax.xml.transform.TransformerFactory
+import javax.xml.transform.dom.DOMSource
+import javax.xml.transform.stream.StreamResult
+
+/**
+ * Disables the `maven-default-http-blocker` mirror in the bundled Maven's `settings.xml`.
+ *
+ * Replaces the obsolete [com.intellij.openapi.startup.StartupActivity.DumbAware] with the modern
+ * [ProjectActivity] which is the recommended way since IntelliJ Platform 2023.1.
+ */
+class DisableMavenDefaultBlockerActivity : ProjectActivity {
+
+    private val LOG = Logger.getInstance(DisableMavenDefaultBlockerActivity::class.java)
+
+    override suspend fun execute(project: Project) {
+        val pluginsPath = PathManager.getPreInstalledPluginsPath()
+        LOG.debug("PreInstalled plugins path $pluginsPath")
+        loadSettings("$pluginsPath/maven/lib/maven3/conf/settings.xml")
+    }
+
+    private fun loadSettings(path: String) {
+        val dbf = DocumentBuilderFactory.newDefaultInstance()
+        try {
+            val db = dbf.newDocumentBuilder()
+            val doc = db.parse(path)
+            val mirrorsNodes = doc.getElementsByTagName("mirrors")
+            if (mirrorsNodes.length == 1) {
+                val mirrors = mirrorsNodes.item(0) ?: return
+                val mirrorNodes = mirrors.childNodes
+                for (i in 0 until mirrorNodes.length) {
+                    val mirror = mirrorNodes.item(i) ?: continue
+                    val childNodes = mirror.childNodes
+                    for (k in 0 until childNodes.length) {
+                        val childNode = childNodes.item(k) ?: continue
+                        if (childNode.nodeName == "id") {
+                            val data = childNode.firstChild
+                            if (data is CharacterData && data.data == "maven-default-http-blocker") {
+                                mirrors.removeChild(mirror)
+                                val tf = TransformerFactory.newInstance()
+                                val transformer = tf.newTransformer()
+                                val source = DOMSource(doc)
+                                val result = StreamResult(File(path))
+                                transformer.transform(source, result)
+                                LOG.info("Success delete the maven-default-http-blocker!")
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (e: Exception) {
+            LOG.error("Delete maven-default-http-blocker error!", e)
+        }
+    }
+}
